c37a2e98347e362c9833d949c90c2410ff8337e9
Syed Jamal <syedjamal969@gmail.com>
2024-12-26 16:38:09 +0500

    ad ids & signed

--- lib/app/utills/app_strings.dart
+++ lib/app/utills/app_strings.dart
@@ -11,17 +11,17 @@ class AppStrings {
   static const MAX_CHAT_TOKKENS = 400;
 
   // static const MAX_BANNER_ID = "d6095034e74ae6a4";
-  static const MAX_BANNER_ID = "84f49d62f65f1efb";
+  static const MAX_BANNER_ID = "d2971c5f7ddca1c8";
 
   // static const MAX_INTER_ID = " e50d9cc101b3fc9f";
-  static const MAX_INTER_ID = " aa37c4358bbfc6a0";
+  static const MAX_INTER_ID = "2fbba7505c9f85f6";
 
-  static const MAX_APPOPEN_ID = "57743e8e7805b726";
+  static const MAX_APPOPEN_ID = "";
 
   // static const MAX_MREC_ID = "b64da6cf308710d4";
-  static const MAX_MREC_ID = "8188be3d6188668a";
+  static const MAX_MREC_ID = "9b935ac0980410e6";
 
-  static const MAX_Reward_ID = "5fd0a59908c97b06";
+  static const MAX_Reward_ID = "ecd7d0fa4ed46a78";
 
   static const MAX_SDK =
       "aRDMGbWzwEWwvtKeNdJsHwxEn46w01GNDRaxtR7ShTArDJv5nR9rzFS-Emmb6e9epRPaiGUXRSM9NGeORoZM5L";
